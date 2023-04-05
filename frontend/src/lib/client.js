7ca46b54f44a8117338681367c671c13e363d870
Nikita Mirolyubov <mirolyubov_n@yahoo.co.uk>
2023-04-05 11:20:43 +0600

    take url from env

--- frontend/src/lib/client.js
+++ frontend/src/lib/client.js
@@ -1,12 +1,12 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-
+const URL = process.env.NODE_ENV === "production" ? process.env.API_URL : process.env.LOCAL_URL;
 let client = null;
 
 export const getClient = () => {
 	if (!client || typeof window === "undefined") {
 		client = new ApolloClient({
 			link: new HttpLink({
-				uri: "http://127.0.0.1:1337/graphql",
+				uri: URL,
 			}),
 			cache: new InMemoryCache(),
 			connectToDevTools: true,
